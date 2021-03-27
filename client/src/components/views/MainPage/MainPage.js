e050b4727a6f17258e6381df29c020f16fe88c58
yujin <claire9585@sookmyung.ac.kr>
2021-03-27 10:30:42 +0900

    4주차 완료

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -7,38 +7,37 @@ class MainPage extends React.Component {
   state = {
     isLoading: true,
     category: "전체",
-    users: []
+    users: [],
   };
   getLists = async () => {
     const { category } = this.state;
-    
+
     let body = {
-      category, 
+      category,
     };
     const response = await axios.post("api/main/getMain", body);
     console.log(response);
-    this.setState({ 
+    this.setState({
       users: response.data.user,
       isLoading: false,
     });
-
   };
   componentDidMount() {
     this.getLists();
   }
-  
+
   render() {
     const { isLoading, users } = this.state;
     return (
       <>
-        <section className="container">  
+        <section className="container">
           {isLoading ? (
             <div className="loader">
               <span className="loader__text">Loading...</span>
             </div>
           ) : (
             <div className="lists">
-              {users.map(user => (
+              {users.map((user) => (
                 <MainList
                   key={user.ID}
                   id={user.ID}
